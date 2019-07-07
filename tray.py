602979e759f9677ca6378e2d788aba7f33d72c60
Rajiv Sharma <rajiv.vfx@gmail.com>
2019-07-07 20:57:24 +0800

    Python System Tray application by PySide2

---
+++ tray.py
@@ -0,0 +1,107 @@
+"""
+Name: System Tray Application
+Author :  Rajiv Sharma
+Developer Website : www.hqvfx.com
+Developer Email   : rajiv@hqvfx.com
+Date Started : 16 sept 2018
+Date Modified :
+Description : Desktop client for Stdio Line Production pipeline
+
+Download Application from : www.hqvfx.com/downloads
+Source Code Website : www.github.com/hqvfx
+Free Video Tutorials : www.youtube.com/vfxpipeline
+
+Copyright (c) 2018, HQVFX(www.hqvfx.com) . All rights reserved.
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+    * Neither the name of HQVFX(www.hqvfx.com) nor the names of any
+      other contributors to this software may be used to endorse or
+      promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+import os
+import sys
+from PySide2 import QtWidgets, QtGui
+
+
+class SystemTrayIcon(QtWidgets.QSystemTrayIcon):
+    """
+    CREATE A SYSTEM TRAY ICON CLASS AND ADD MENU
+    """
+    def __init__(self, icon, parent=None):
+        QtWidgets.QSystemTrayIcon.__init__(self, icon, parent)
+        self.setToolTip(f'VFX Pipeline Application Build - 3.2.56')
+        menu = QtWidgets.QMenu(parent)
+        open_app = menu.addAction("Open Notepad")
+        open_app.triggered.connect(self.open_notepad)
+        open_app.setIcon(QtGui.QIcon("icon.png"))
+
+        open_cal = menu.addAction("Open Calculator")
+        open_cal.triggered.connect(self.open_calc)
+        open_cal.setIcon(QtGui.QIcon("icon.png"))
+
+        exit_ = menu.addAction("Exit")
+        exit_.triggered.connect(lambda: sys.exit())
+        exit_.setIcon(QtGui.QIcon("icon.png"))
+
+        menu.addSeparator()
+        self.setContextMenu(menu)
+        self.activated.connect(self.onTrayIconActivated)
+
+    def onTrayIconActivated(self, reason):
+        """
+        This function will trigger function on click or double click
+        :param reason:
+        :return:
+        """
+        if reason == self.DoubleClick:
+            self.open_notepad()
+        # if reason == self.Trigger:
+        #     self.open_notepad()
+
+    def open_notepad(self):
+        """
+        this function will open application
+        :return:
+        """
+        os.system('notepad')
+
+    def open_calc(self):
+        """
+        this function will open application
+        :return:
+        """
+        os.system('calc')
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    w = QtWidgets.QWidget()
+    tray_icon = SystemTrayIcon(QtGui.QIcon("icon.png"), w)
+    tray_icon.show()
+    tray_icon.showMessage('VFX Pipeline', 'Hello "Name of logged in ID')
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
